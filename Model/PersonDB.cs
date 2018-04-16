8d4d9025d15b3bb3e6d195284aa4b341b5fdc63b
Jopno <35934376+Jopno@users.noreply.github.com>
2018-04-16 14:06:44 +0300

    Add files via upload

---
+++ Model/PersonDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace MVVMApplication.Model
+{
+    public class PersonDB : Person
+    {
+        public PersonDB() : base("name=Person")
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
